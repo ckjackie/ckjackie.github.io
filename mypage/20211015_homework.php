955a2df93ab8aaaadbd9f68b17eb1606cfe79e7a
ckjackie <wch0212@gmail.com>
2021-10-18 08:40:32 +0800

    Add files via upload

---
+++ mypage/20211015_homework.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Your request method is  " . $_SERVER['REQUEST_METHOD'] . "<br>";
+echo "Host http is  " . $_SERVER['HTTP_HOST'] . "<br>";
+echo "Sever name is  " . $_SERVER['SERVER_NAME'] . "<br>";
+echo "Request uri is  " . $_SERVER['REQUEST_URI'] . "<br>";
+echo "PHP page name is  " . $_SERVER['PHP_SELF'] . "<br>";
+echo "Server protocal is  " . $_SERVER['SERVER_PROTOCOL'] . "<br>";
+echo "Server port is  " . $_SERVER['SERVER_PORT'] . "<br>";
+echo "Server admin is  " . $_SERVER['SERVER_ADMIN'] . "<br>";
+echo "Remote addr is  " . $_SERVER['REMOTE_ADDR'] . "<br>";
+
+?>
+
